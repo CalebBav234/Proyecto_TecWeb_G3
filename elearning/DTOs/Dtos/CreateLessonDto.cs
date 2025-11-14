7d57333cbece96a888e882984501a3969f61045d
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 13:20:29 -0400

    Feat: added CreateLessonDto

---
+++ elearning/DTOs/Dtos/CreateLessonDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CreateLessonDto
+{
+    [Required]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public string Content { get; set; } = null!;
+
+    [Required]
+    public int CourseId { get; set; }
+}
--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b341a8f3bd6c12ed55481d1ed67f6ec0204a4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f908838ebf94c608f3b903356e7151104c0b442")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
