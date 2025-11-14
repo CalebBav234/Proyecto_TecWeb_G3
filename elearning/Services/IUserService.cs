4ce155dc91a7276d7c183dce871d627714955d0d
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 15:36:01 -0400

    feat: deleted TODO file

---
+++ elearning/Services/IUserService.cs
@@ -0,0 +1,15 @@
+using DTOs;
+using DTOs.Dtos;
+
+namespace Services;
+
+public interface IUserService
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByRefreshToken(string refreshToken);
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User> AddAsync(RegisterUserDto dto);
+    Task<User> UpdateAsync(UserDto dto);
+    Task RemoveAsync(Guid id);
+}
---
+++ elearning/Services/UserService.cs
@@ -0,0 +1,70 @@
+using DTOs;
+using DTOs.Dtos;
+using Repositories;
+using AutoMapper;
+
+namespace Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _repo;
+    private readonly IMapper _mapper;
+
+    public UserService(IUserRepository repo, IMapper mapper)
+    {
+        _repo = repo;
+        _mapper = mapper;
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _repo.GetByIdAsync(id);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _repo.GetByEmailAsync(email);
+    }
+
+    public async Task<User?> GetByRefreshToken(string refreshToken)
+    {
+        return await _repo.GetByRefreshToken(refreshToken);
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _repo.GetAllAsync();
+    }
+
+    public async Task<User> AddAsync(RegisterUserDto dto)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Username = dto.Username,
+            Email = dto.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+            Role = "User"
+        };
+        await _repo.AddAsync(user);
+        return user;
+    }
+
+    public async Task<User> UpdateAsync(UserDto dto)
+    {
+        User? user = await GetByIdAsync(dto.Id);
+        if (user == null) throw new Exception("User doesnt exist.");
+        user.Username = dto.Username;
+        user.Email = dto.Email;
+        await _repo.UpdateAsync(user);
+        return user;
+    }
+
+    public async Task RemoveAsync(Guid id)
+    {
+        User? user = await GetByIdAsync(id);
+        if (user == null) return;
+
+        await _repo.RemoveAsync(user);
+    }
+}
