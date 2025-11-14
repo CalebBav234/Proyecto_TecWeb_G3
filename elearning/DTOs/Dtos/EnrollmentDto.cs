011948fd14ed6be9ca42f91ea7f243a4e1094e57
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 13:31:04 -0400

    Feat: added EnrollmentDto.cs

---
+++ elearning/DTOs/Dtos/EnrollmentDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+public class EnrollmentDto
+{
+    public int UserId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public DateTime EnrolledAt { get; set; }
+
+    [Range(0, 100)]
+    public float Progress { get; set; }
+
+    public UserDto User { get; set; } = null!;
+
+    public CourseDto Course { get; set; } = null!;
+}
--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d57333cbece96a888e882984501a3969f61045d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc4ca6bd39d3bf4eafeeded71f55cfd5b47d54be")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
