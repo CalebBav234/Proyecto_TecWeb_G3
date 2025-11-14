ee04dd3e997b72cf463dafc09c9b48640d152a9a
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 15:37:47 -0400

    Feat: create UserService

--- elearning/Services/IUserService.cs
+++ elearning/Services/IUserService.cs
@@ -9,6 +9,7 @@ public interface IUserService
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByRefreshToken(string refreshToken);
     Task<IEnumerable<User>> GetAllAsync();
+    
     Task<User> AddAsync(RegisterUserDto dto);
     Task<User> UpdateAsync(UserDto dto);
     Task RemoveAsync(Guid id);
