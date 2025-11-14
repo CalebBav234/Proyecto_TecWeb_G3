a9b341a8f3bd6c12ed55481d1ed67f6ec0204a4b
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 13:10:38 -0400

    Feat: added UpdateCourseDto

---
+++ elearning/DTOs/Dtos/UpdateCourseDto.cs
@@ -0,0 +1,8 @@
+public class UpdateCourseDto
+{
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? TeacherId { get; set; }
+}
--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1857d41bc87bfa64c0770dc03a674071c81fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d37f3866e2ef470e18021f0dba03a01acff975")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
