e2e217f36be9c5b7deef8f8399233af63a0b3ada
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 12:09:41 -0400

    added UserDto

---
+++ elearning/DTOs/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+public class UserDto
+{
+    public int Id { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    public string Role { get; set; } = "User";
+
+    public ProfileDto? Profile { get; set; }
+}
--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1672d41bf655fc14f703f6bb30082e040f7f34e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19cc12e71d11271fde60c7d76d978c1a1190030")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
