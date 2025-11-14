d88750354c98397dcc0af50ce97150d6c7cd000c
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 09:46:47 -0400

    Feat: added entity user to dbcontext

---
+++ elearning/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using DTOs;
+using Microsoft.EntityFrameworkCore;
+namespace Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users => Set<User>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+         modelBuilder.Entity<User>()
+            .HasOne(u => u.Profile)
+            .WithOne(p => p.User)
+            .HasForeignKey<Profile>(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
