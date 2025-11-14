a1eddd9591f42c6912998a4dc5cd5d2eb45ec52e
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 09:32:19 -0400

    Feat: created entity Course

--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d7f38068f6dfb236db7204e6b2f63d4b1ddb22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d360dd14240c9a1dbb9ab18981545ac34a557a")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
