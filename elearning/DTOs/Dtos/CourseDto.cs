de1857d41bc87bfa64c0770dc03a674071c81fc1
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 13:04:06 -0400

    added CourseDto

---
+++ elearning/DTOs/Dtos/CourseDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+public class CourseDto
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int TeacherId { get; set; }
+
+    public UserDto Teacher { get; set; } = null!;
+
+    public List<LessonDto> Lessons { get; set; } = new();
+
+    public List<EnrollmentDto> Enrollments { get; set; } = new();
+}
--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+577a3753be2c4100ff5e23b2f624e597b9ae512c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce138c5b67faad063395fd2f3e883a86dadbb2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
