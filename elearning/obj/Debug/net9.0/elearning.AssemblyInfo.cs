ef4c02598058766365519716abdf3095678768ff
CalebBav234 <calebmbaving@gmail.com>
2025-11-14 10:09:03 -0400

    Feat: added the interface for User Repository

--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d7f11d52fcdc90afb56414bc1251a3f3d8327f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1748faade34ac6187460e821a63ea47649447df4")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
