e509293e44a8d6709129d771546dcad7427517b4
CalebBav234 <calebmbaving@gmail.com>
2025-11-18 12:36:22 -0400

    Refact: updated docker files and app settings

--- elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
+++ elearning/obj/Debug/net9.0/elearning.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+901a18f51efda90792ff39f15f872fca2a62b68a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6375e61cba70fd807b8146687bd414a086017437")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
